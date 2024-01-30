b3952c082d070b7e10aec1ba9b9a7efe932bfb0d
jxu96 <garyxu1001@gmail.com>
2024-01-30 15:16:26 +0000

    feat: auto capture and testing configuration

---
+++ capture.sh
@@ -0,0 +1,28 @@
+INTERFACE="$1"
+
+if [ -z "${INTERFACE}" ]; then
+    echo "Undefined Interface."
+    exit 1
+fi
+
+if [ -f "./capture.pcapng" ]; then
+    rm ./capture.pcapng
+fi
+
+echo "Starting nr-gnb .."
+./build/nr-gnb -c open5gs-gnb.yaml > /dev/null 2>&1 &
+pid_gnb=$!
+sleep 1s
+
+echo "TShark capturing .."
+tshark -i ${INTERFACE} -F pcapng -w ./capture.pcapng > /dev/null 2>&1 &
+pid_tshark=$!
+sleep 1s
+
+echo "Starting nr-ue .."
+sudo bash -c './build/nr-ue -c open5gs-ue.yaml > /dev/null 2>&1 &'
+sleep 1s
+
+kill ${pid_tshark}
+sudo kill $(ps aux | grep nr-ue | awk '{print $2}')
+kill ${pid_gnb}
